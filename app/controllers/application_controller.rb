61a31ddc1a05da90457a4b70a9de42554064f433
Andrew Wirt <awjwirt@gmail.com>
2018-07-17 18:55:02 +0000

    test push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < Sinatra::Base
   get '/' do
     return erb :index
   end
+  
+  post '/' do
+    @the_fortune = get_fortune
+    @the_user = params[:user]
+    return erb :results
+  end
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,14 @@
-class SampleModel
-  attr_accessor :name, :age
+def get_fortune
+  
+  fortunes = ["you will find the love of your life tomorrow!",
+              "good things are coming your way!",
+              "get out of the country immediately!",
+              "go to saunatherapy to expel your toxins!",
+              "a penguin will show up at your door at noon soliciting donations.",
+              "you're going to breathe in like three seconds.",
+              "OOoOoOOooooOOOOOOOOOoooOooOOo SPIIIIIIIIIIIIICYyyyyYYyYYyy."
+              ]
+              
+  return fortunes.sample
+  
 end
